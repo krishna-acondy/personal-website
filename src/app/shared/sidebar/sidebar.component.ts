3c8d144d9da2cabbc4e492d1a57115ea0612f8e3
krishna-acondy <krishnachaitanya.a3@gmail.com>
2018-05-07 22:02:23 +0100

    Color selected menu item, close menubar on item click

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -10,9 +11,28 @@ export class SidebarComponent implements OnInit {
   @Input()
   height: string;
 
-  constructor() { }
+  @Output()
+  linkClick = new EventEmitter();
+
+  links = [
+    { class: '', text: 'Home', target: '/'},
+    { class: 'web-development', text: 'Web Development', target: '/web-development'},
+    { class: 'photography', text: 'Photography', target: '/photography'},
+    { class: 'design', text: 'Design', target: '/design'}
+  ];
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  isSelected(route: string) {
+    return this.router.url === `${route}`;
+  }
+
+  navigateTo(target: string) {
+    this.router.navigateByUrl(target);
+    this.linkClick.emit();
   }
 
 }
